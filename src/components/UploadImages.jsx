66778d1f3186e10a17128ef32d1814aa1bdfa723
Adarsh <adarshkumar102004@gmail.com>
2025-03-07 00:41:35 +0530

    upload images functionality

--- src/components/UploadImages.jsx
+++ src/components/UploadImages.jsx
@@ -4,12 +4,17 @@ const UploadImages = () => {
     return (
         <div className='w-full items-center'>
 
-            <form className='w-[50%] flex flex-col mx-auto  rounded border-2 border-gray-700 p-4 mt-10'>
+            <form className='w-[100%] flex flex-col mx-auto  rounded border-2 border-gray-700 p-4 mt-10'>
                 <h2 className='font-bold text-2xl mb-2'>Upload Images</h2>
                 <div className='flex justify-between w-[100%]'>
-                    <div className='flex flex-col mb-2'>
-                        <p className='font-semibold'>Product Name</p>
-                    </div>
+                    <div>
+                        <label htmlFor='upload-image'>
+                          <div className='bg-green-500 hover:cursor-pointer hover:shadow-md shadow-black hover:bg-opacity-45 transition duration-150 font-bold text-2xl border-black border-2 rounded-md h-[10rem] bg-opacity-30 flex justify-center content-center'>
+                            <p className='m-auto'>+</p>
+                          </div>
+                        </label>
+                        <input type='file' multiple={true} id='upload-image' className='opacity-0'/>
+                   </div>
                 </div>
             </form>
         </div>
